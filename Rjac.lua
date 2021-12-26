ca4a1387e802d2d9b88e2fd994aef4b7e31f63f8
Giant <83135006+Giant427@users.noreply.github.com>
2021-12-27 00:45:40 +0530

    Trying out moonwave

--- Rjac.lua
+++ Rjac.lua
@@ -8,6 +8,11 @@
 -- Rjac --
 ----------
 
+--[=[
+    @class Rjac
+
+    This is the main Rjac class
+]=]
 local Rjac = {}
 
 ---------------
@@ -27,6 +32,9 @@ Rjac.Enabled = false
 
 -- Initiate
 
+--[=[
+    Basically start the processing of the Object.
+]=]
 function Rjac:Initiate()
 	self.Character = self.Player.Character
 
@@ -38,6 +46,10 @@ function Rjac:Initiate()
 end
 
 -- Character added
+
+--[=[
+	Rotates the body joints once, according to the camera direction.
+]=]
 
 function Rjac:CharacterAdded(Character)
 	Character:WaitForChild("Humanoid")
@@ -60,6 +72,9 @@ end
 
 -- Update character
 
+--[=[
+	Rotates the body joints once, according to the camera direction.
+]=]
 function Rjac:UpdateCharacter()
 	if not self.Enabled then return end
 
@@ -90,6 +105,11 @@ end
 
 -- Update body position
 
+--[=[
+	Updates the direction of where the character is supposed to look at.
+
+	@param CameraCFrame CFrame -- The camera CFrame of the player
+]=]
 function Rjac:UpdateDirection(CameraCFrame)
 	if not self.Character then
 		warn("Character does not exist for Player:", self.Player.Name)
@@ -110,6 +130,13 @@ end
 do
 	-- Add body joint
 
+	--[=[
+		Adds a body joint which will be affected by the Object.
+
+		@param BodyPart string -- The name of the part which will be affected
+		@param BodyJoint string -- The name of the joint which will be rotated
+		@param MultiplierVector Vector3 -- The value which affects the rotations
+	]=]
 	function Rjac:AddBodyJoint(BodyPart, BodyJoint, MultiplierVector)
 		for _,v in pairs(self.Configurations) do
 			if v.BodyPart == BodyPart and v.BodyJoint == BodyJoint then
@@ -147,6 +174,12 @@ do
 
 	-- Remove body joint
 
+	--[=[
+		Removes a body joint which will be affected by the Object.
+
+		@param BodyPart string -- The name of the part which will be affected
+		@param BodyJoint string -- The name of the joint which will be rotated
+	]=]
 	function Rjac:RemoveBodyJoint(BodyPart, BodyJoint)
 		-- Remove and store configuration
 
@@ -181,6 +214,13 @@ end
 do
 	-- Update body joint offset
 
+	--[=[
+		Updates the joint offset configuration of the given BodyJoint. Can be used when the character is being rotated but the body parts still need to face the camera direction.
+
+		@param BodyPart string -- The name of the part which will be affected
+		@param BodyJoint string -- The name of the joint which will be rotated
+		@param JointOffset CFrame -- The offset by which the joint will be rotated
+	]=]
 	function Rjac:UpdateBodyJointOffset(BodyPart, BodyJoint, JointOffset)
 		for _,v in pairs(self.Configurations) do
 			if v.BodyPart == BodyPart and v.BodyJoint == BodyJoint then
@@ -192,6 +232,13 @@ do
 
 	-- Update body joint multiplier vector
 
+	--[=[
+		Adds a body joint which will be affected by the Object.
+
+		@param BodyPart string -- The name of the part which will be affected
+		@param BodyJoint string -- The name of the joint which will be rotated
+		@param MultiplierVector Vector3 -- The value which affects the rotations
+	]=]
 	function Rjac:UpdateBodyJointMultiplierVector(BodyPart, BodyJoint, MultiplierVector)
 		for _,v in pairs(self.Configurations) do
 			if v.BodyPart == BodyPart and v.BodyJoint == BodyJoint then
@@ -204,6 +251,9 @@ end
 
 -- Destroy
 
+--[=[
+	Destroy the profile
+]=]
 function Rjac:Destroy()
 	self.Enabled = false
 
@@ -230,12 +280,23 @@ end
 -- Rjac module --
 -----------------
 
+--[=[
+    @class RjacModule
+
+    This is the module which build the RjacProfile for the player
+]=]
 local RjacModule = {}
 
 -----------------
 -- Constructor --
 -----------------
 
+--[=[
+	Destroy the profile
+
+	@param ProfileInfo table -- The data which will be overwritten while returning the class
+	@return RjacProfile table -- Returns the RjacProfile which has bee fully creating according to the provided ProfileInfo
+]=]
 function RjacModule:New(ProfileInfo)
 	ProfileInfo = ProfileInfo or {}
 	setmetatable(ProfileInfo, Rjac)
